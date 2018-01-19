3e1386c337c7dcb19df09a6ba34182f865e1a22d
karimchukfeh <karimchukfeh@gmail.com>
2018-01-19 17:21:13 -0500

    progress

--- Exporter.php
+++ Exporter.php
@@ -12,33 +12,57 @@ class Exporter{
   protected $text; // A string
   protected $images; // An array of strings
 
-  function __construct($name, $textString, $imagesString){
+  function __construct($name, $text, $imagesString){
 
-    // Sanitizes $name to be a legible file name
-    $cleanName = preg_replace("/[^a-zA-Z]/", "", $name);
+    // Sanitizes $name to be a good file name
+    $this->zipName = "./".preg_replace("/[^a-zA-Z]/", "", $name).".zip";
 
-    $this->zipName = "./".$this->$cleanName.".zip";
+    // Create a zip file
     $this->zip = new ZipArchive();
-    $this->inputText = $text;
-    $this->images = explode(",", $imagesString);
+
+    // Prepare the data
+    $this->text = $text;
+    $this->images = explode(" ", $imagesString);
+
+  }
+
+  function openZip(){
+    if ($this->zip->open($this->zipName, ZipArchive::CREATE)!==TRUE) {
+      exit("cannot open <$this->zipName>\n");
+    }
   }
 
   function addTextToZip(){
-
+    $this->openZip();
+    $file = "summary.txt";
+    $this->zip->addFromString($file, $this->text);
+    $this->zip->close;
   }
 
 
   function addImagesToZip(){
-
+    $this->openZip();
+    foreach($this->images as $imgUrl){
+      if(copy($imgUrl, "tmp/".basename($imgUrl))){
+        if(file_exists(basename($imgUrl))){
+          $this->zip->addFile("tmp/".basename($imgUrl));
+        }
+      }else{
+        print 'Failed to copy !!!';
+      }
+    }
   }
 
-  function downloadZip(){
-
+  function deleteTemps(){
+    delete("tmp");
   }
+
 
 }
 
-
-
+$exporter = new Exporter("myzip", "Name: Shrek\nRole: Green Candidate", "https://images.moviepilot.com/images/c_limit,q_auto:good,w_600/m5xa5ajsxsflc2gbdy6k/shrek-credit-dreamworks-pictures.jpg http://shrekshrekshrek.weebly.com/uploads/3/1/0/9/31093949/2456051.jpg");
+$exporter->addTextToZip();
+$exporter->addImagesToZip();
+$exporter->deleteTemps();
 
 ?>
