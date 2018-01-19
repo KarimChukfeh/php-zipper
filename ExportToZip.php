56654e1a7cd3ee382266926d3da1c1ab778c80fd
karimchukfeh <karimchukfeh@gmail.com>
2018-01-19 11:59:08 -0500

    initial commit

---
+++ ExportToZip.php
@@ -0,0 +1,43 @@
+<?php
+/*
+  Author: Karim Chukfeh
+  01/19/2018
+*/
+
+
+class ExportToZip{
+
+  protected $zip;
+  protected $zipName;
+  protected $text; // A string
+  protected $images; // An array of strings
+
+  function __construct($name, $textString, $imagesString){
+
+    // Sanitizes $name to be a legible file name
+    $cleanName = preg_replace("/[^a-zA-Z]/", "", $name);
+
+    $this->zipName = "./".$this->$cleanName.".zip";
+    $this->zip = new ZipArchive();
+    $this->inputText = $text;
+    $this->images = explode(",", $imagesString);
+  }
+
+  function addTextToZip(){
+
+
+  }
+
+
+  function addImagesToZip(){
+
+
+  }
+
+
+}
+
+
+
+
+?>
