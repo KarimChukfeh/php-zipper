a10f139e48fce2341f4c02afc3c4a96ea1a3c2f4
KarimChukfeh <kchuk030@uottawa.ca>
2018-01-23 05:07:01 -0500

    update readme

--- BioToZip.php
+++ BioToZip.php
@@ -3,26 +3,26 @@
 
 class BioToZip{
 
-  protected $name;
-  protected $text;
+  protected $fil3Name;
+  protected $summary;
   protected $images;
 
   protected $zip;
 
-  function __construct($name, $text, $images){
+  function __construct($name, $summary, $images){
 
     //////////////////////
     // Constructor area //
     //////////////////////
 
     // The string to become .txt
-    $this->text = $text;
+    $this->$summary = $text;
 
     // The images are an array of URLs parsed from a string of URLs
     $this->images = explode(" ", $images);
 
     // The directory name is $name but sanatized
-    $this->name = preg_replace('/[^a-zA-Z0-9\-\._]/','', $name);
+    $this->fileName = preg_replace('/[^a-zA-Z0-9\-\._]/','', $name);
 
     ///////////////////////
     // Main routine area //
@@ -33,24 +33,24 @@ class BioToZip{
       mkdir("temp", 0777, true);
     }
 
-    // 2) Create Bio.txt and add it to the temporary directory
-    file_put_contents("temp/Bio.txt", $this->text, FILE_APPEND | LOCK_EX);
+    // 2) Create summary.txt and add it to the temporary directory
+    file_put_contents("temp/summary.txt", $this->$summary, FILE_APPEND | LOCK_EX);
 
     // 3) Fetch the images one by one and add them to the temporary directoy
     foreach($this->images as $imgUrl){
       if(! copy($imgUrl, "temp/".basename($imgUrl))){
-        print 'Failed to copy !!!';
+        print "Failed to copy $imgUrl";
       }
     }
 
     // 4) Make an empty $name.zip folder
     $this->zip = new ZipArchive();
-    $createZip = $this->zip->open($this->name.".zip", ZipArchive::CREATE);
+    $createZip = $this->zip->open($this->fileName.".zip", ZipArchive::CREATE);
 
     // 5) populate $name.zip with all files in the temporary directory
     $rootPath = realpath('temp/');
     if ($createZip !== TRUE) {
-      exit("cannot open <$this->name>\n");
+      exit("cannot open <$this->fileName>\n");
     }
     $files = new RecursiveIteratorIterator(
         new RecursiveDirectoryIterator($rootPath),
@@ -78,7 +78,11 @@ class BioToZip{
 
 }
 
-// Uncomment for an example
-// $example = new BioExport("GBreen Candidate #1", "Name: Shrek\nRole: Green Candidate", "https://images.moviepilot.com/images/c_limit,q_auto:good,w_600/m5xa5ajsxsflc2gbdy6k/shrek-credit-dreamworks-pictures.jpg http://shrekshrekshrek.weebly.com/uploads/3/1/0/9/31093949/2456051.jpg");
+// Uncomment below for an example
+
+// $fileName = "Green Candidate #1";
+// $summary = "Name: Shrek\nRole: Green Candidate";
+// $images = "https://images.moviepilot.com/images/c_limit,q_auto:good,w_600/m5xa5ajsxsflc2gbdy6k/shrek-credit-dreamworks-pictures.jpg http://shrekshrekshrek.weebly.com/uploads/3/1/0/9/31093949/2456051.jpg";
+// $generateZip = new BioToZip($fileName, $summary, $images);
 
 ?>
